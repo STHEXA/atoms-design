81cd3730549bfada1e68bab1e21c1783da28ea28
STHEXA <ti.syu.s@gmail.com>
2024-03-12 23:17:44 +0900

    AtomicDesignの残り分

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
-
-const user = {
-  name: '太郎',
-  image: 'https://source.unsplash.com/black-pug-with-gray-knit-scarf-Mv9hjnEUHR4',
-  email: '12345@example.com',
-  phone: '090-1234-5678',
-  company: {
-    name: 'test株式会社'
-  },
-  website: 'https://google.com'
-}
+import { Router } from './router/Router';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <PrimaryButton>test</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user}/>
-    </div>
+    <Router />
   );
 }
 
